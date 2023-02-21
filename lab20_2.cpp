6dd56be04ae198a9c77eaaeebe02e118511481db
nnrwp <rawipa_s@cmu.ac.th>
2023-02-21 16:29:11 +0700

    completed

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,9 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *r1, Rect *r2){
+    double wo = min((r2->x + r2->w),(r1->x + r1->w)) - max(r2->x,r1->x);
+    double ho = min(r1->y,r2->y) - max((r2->y - r2->h),(r1->y - r1->h));
+    if(wo < 0 || ho < 0) return 0;
+    else return wo*ho;
 }
